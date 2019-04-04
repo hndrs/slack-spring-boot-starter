6b4e29b3bf26324ba1d5609cf12893b3133f7fb3
Ben Denger <ben@kreait.com>
2019-04-04 17:06:06 +0200

    added mockMethod, tests, test-extensions

---
+++ data/slack-jackson-dto-test-extensions/src/main/kotlin/io/olaph/slack/dto/jackson/group/oauth/OauthAccessExtensions.kt
@@ -0,0 +1,24 @@
+package io.olaph.slack.dto.jackson.group.oauth
+
+
+fun OauthAccessRequest.Companion.sample(): OauthAccessRequest {
+    return OauthAccessRequest("", "", "")
+}
+
+fun Bot.Companion.sample(): Bot {
+    return Bot("", "")
+}
+
+fun IncomingWebhook.Companion.sample(): IncomingWebhook {
+    return IncomingWebhook("", "", "", "")
+}
+
+fun SuccessFullOauthAccessResponse.Companion.sample(): SuccessFullOauthAccessResponse {
+    return SuccessFullOauthAccessResponse(true, "", "", "", "", "",
+            IncomingWebhook.sample(),
+            Bot.sample())
+}
+
+fun ErrorOauthAccessResponse.Companion.sample(): ErrorOauthAccessResponse {
+    return ErrorOauthAccessResponse(false, "")
+}
