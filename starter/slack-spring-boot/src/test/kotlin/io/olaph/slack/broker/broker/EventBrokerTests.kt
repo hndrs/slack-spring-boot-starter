c94135991687e4d8883f0af659bfcb3f11a9d3fd
Marvin Schramm <marvin@kreait.com>
2019-04-12 08:37:28 +0200

    add event broker tests

--- starter/slack-spring-boot/src/test/kotlin/io/olaph/slack/broker/broker/EventBrokerTests.kt
+++ starter/slack-spring-boot/src/test/kotlin/io/olaph/slack/broker/broker/EventBrokerTests.kt
@@ -2,46 +2,45 @@ package io.olaph.slack.broker.broker
 
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import io.olaph.slack.broker.extensions.sample
-import io.olaph.slack.broker.metrics.CommandMetrics
-import io.olaph.slack.broker.receiver.SlashCommandReceiver
+import io.olaph.slack.broker.metrics.EventMetrics
+import io.olaph.slack.broker.receiver.EventReceiver
 import io.olaph.slack.broker.store.InMemoryTeamStore
 import io.olaph.slack.broker.store.Team
-import io.olaph.slack.dto.jackson.SlackCommand
+import io.olaph.slack.dto.jackson.SlackEvent
 import io.olaph.slack.dto.jackson.sample
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpHeaders
 
-@DisplayName("Command Broker Tests")
-class CommandBrokerTests {
+@DisplayName("Event Broker Tests")
+class EventBrokerTests {
 
 
     @Test
     @DisplayName("Broker Test")
     fun brokerTest() {
 
-
         val teamStore = InMemoryTeamStore()
 
         teamStore.put(Team.sample().copy(teamId = "TestId"))
 
-        val commandMetrics = CommandMetrics()
+        val metrics = EventMetrics()
 
         val meterRegistry = SimpleMeterRegistry()
-        commandMetrics.bindTo(meterRegistry)
+        metrics.bindTo(meterRegistry)
 
         val successReceiver = SuccessReceiver()
         val errorReceiver = ErrorReceiver()
 
-        CommandBroker(listOf(successReceiver, errorReceiver), teamStore, commandMetrics).receiveCommand(SlackCommand.sample().copy(teamId = "TestId"), HttpHeaders.EMPTY)
+        EventBroker(listOf(successReceiver, errorReceiver), teamStore, metrics).receiveEvents(SlackEvent.sample().copy(teamId = "TestId"), HttpHeaders.EMPTY)
 
         Assertions.assertTrue(successReceiver.executed)
         Assertions.assertTrue(errorReceiver.executed)
 
-        Assertions.assertEquals(1.0, meterRegistry.get("slack.commands.received").counter().count())
-        Assertions.assertEquals(2.0, meterRegistry.get("slack.commands.receiver.executions").counter().count())
-        Assertions.assertEquals(1.0, meterRegistry.get("slack.commands.receiver.errors").counter().count())
+        Assertions.assertEquals(1.0, meterRegistry.get("slack.events.received").counter().count())
+        Assertions.assertEquals(2.0, meterRegistry.get("slack.events.receiver.executions").counter().count())
+        Assertions.assertEquals(1.0, meterRegistry.get("slack.events.receiver.errors").counter().count())
     }
 
 
@@ -53,30 +52,36 @@ class CommandBrokerTests {
 
         teamStore.put(Team.sample().copy(teamId = "TestId"))
 
+        val metrics = EventMetrics()
+
+        val meterRegistry = SimpleMeterRegistry()
+        metrics.bindTo(meterRegistry)
+
         val successReceiver = SuccessReceiver()
         val errorReceiver = ErrorReceiver()
 
-        CommandBroker(listOf(successReceiver, errorReceiver), teamStore).receiveCommand(SlackCommand.sample().copy(teamId = "TestId"), HttpHeaders.EMPTY)
+        EventBroker(listOf(successReceiver, errorReceiver), teamStore, metrics).receiveEvents(SlackEvent.sample().copy(teamId = "TestId"), HttpHeaders.EMPTY)
 
         Assertions.assertTrue(successReceiver.executed)
         Assertions.assertTrue(errorReceiver.executed)
     }
 
-    class SuccessReceiver : SlashCommandReceiver {
+    class SuccessReceiver : EventReceiver {
 
         var executed: Boolean = false
 
-        override fun onReceiveSlashCommand(slackCommand: SlackCommand, headers: HttpHeaders, team: Team) {
+        override fun onReceiveEvent(slackEvent: SlackEvent, headers: HttpHeaders, team: Team) {
             executed = true
         }
 
+
     }
 
-    class ErrorReceiver : SlashCommandReceiver {
+    class ErrorReceiver : EventReceiver {
 
         var executed: Boolean = false
 
-        override fun onReceiveSlashCommand(slackCommand: SlackCommand, headers: HttpHeaders, team: Team) {
+        override fun onReceiveEvent(slackEvent: SlackEvent, headers: HttpHeaders, team: Team) {
             executed = true
             throw IllegalStateException("Failing Test Case")
         }
