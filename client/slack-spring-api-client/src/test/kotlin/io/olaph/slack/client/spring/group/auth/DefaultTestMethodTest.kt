3536ddcbb6e9f1efe7ba131773157306568b3a22
Marvin Schramm <marvin@kreait.com>
2019-04-17 19:01:18 +0200

    fixes response verification on auth group methods

--- client/slack-spring-api-client/src/test/kotlin/io/olaph/slack/client/spring/group/auth/DefaultTestMethodTest.kt
+++ client/slack-spring-api-client/src/test/kotlin/io/olaph/slack/client/spring/group/auth/DefaultTestMethodTest.kt
@@ -1,11 +1,11 @@
 package io.olaph.slack.client.spring.group.auth
 
 import io.olaph.slack.client.spring.MockServerHelper
+import io.olaph.slack.client.spring.Verifier
 import io.olaph.slack.client.spring.group.RestTemplateFactory
 import io.olaph.slack.dto.jackson.group.auth.ErrorAuthTestResponse
 import io.olaph.slack.dto.jackson.group.auth.SuccessfulAuthTestResponse
 import io.olaph.slack.dto.jackson.group.auth.sample
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -25,11 +25,13 @@ class DefaultTestMethodTest {
     fun authTestFailure() {
         val response = ErrorAuthTestResponse.sample()
         val mockServer = MockServerHelper.buildMockRestServer(mockTemplate, response, "auth.test")
+        val verifier = Verifier(response)
 
         DefaultTestMethod("", mockTemplate)
-                .onFailure { Assertions.assertEquals(response, it) }
+                .onFailure { verifier.set(it) }
                 .invoke()
         mockServer.verify()
+        verifier.verify()
     }
 
     @Test
@@ -37,10 +39,12 @@ class DefaultTestMethodTest {
     fun authTestSuccess() {
         val response = SuccessfulAuthTestResponse.sample()
         val mockServer = MockServerHelper.buildMockRestServer(mockTemplate, response, "auth.test")
+        val verifier = Verifier(response)
 
         DefaultTestMethod("", mockTemplate)
-                .onSuccess { Assertions.assertEquals(response, it) }
+                .onSuccess { verifier.set(it) }
                 .invoke()
         mockServer.verify()
+        verifier.verify()
     }
 }
