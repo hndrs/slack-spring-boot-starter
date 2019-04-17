b98cb984a23320956a6994c9d96c452b30d36abf
Marvin Schramm <marvin@kreait.com>
2019-04-17 18:56:33 +0200

    fixes response verification on chat group methods

--- client/slack-spring-api-client/src/test/kotlin/io/olaph/slack/client/spring/group/chat/DefaultUpdateMethodTest.kt
+++ client/slack-spring-api-client/src/test/kotlin/io/olaph/slack/client/spring/group/chat/DefaultUpdateMethodTest.kt
@@ -1,12 +1,12 @@
 package io.olaph.slack.client.spring.group.chat
 
 import io.olaph.slack.client.spring.MockServerHelper
+import io.olaph.slack.client.spring.Verifier
 import io.olaph.slack.client.spring.group.RestTemplateFactory
 import io.olaph.slack.dto.jackson.group.chat.ErrorChatUpdateResponse
 import io.olaph.slack.dto.jackson.group.chat.SlackChatUpdateRequest
 import io.olaph.slack.dto.jackson.group.chat.SuccessfulChatUpdateResponse
 import io.olaph.slack.dto.jackson.group.chat.sample
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -26,13 +26,15 @@ class DefaultUpdateMethodTest {
     fun chatUpdateFailure() {
         val response = ErrorChatUpdateResponse.sample()
         val mockServer = MockServerHelper.buildMockRestServer(mockTemplate, response, "chat.update")
+        val verifier = Verifier(response)
 
         DefaultUpdateMethod("", mockTemplate)
                 .with(SlackChatUpdateRequest.sample())
-                .onFailure { Assertions.assertEquals(response, it) }
+                .onFailure { verifier.set(it) }
                 .onSuccess { }
                 .invoke()
         mockServer.verify()
+        verifier.verify()
     }
 
     @Test
@@ -40,12 +42,13 @@ class DefaultUpdateMethodTest {
     fun chatUpdateSuccess() {
         val response = SuccessfulChatUpdateResponse.sample()
         val mockServer = MockServerHelper.buildMockRestServer(mockTemplate, response, "chat.update")
+        val verifier = Verifier(response)
 
         DefaultUpdateMethod("", mockTemplate)
                 .with(SlackChatUpdateRequest.sample())
-                .onFailure { Assertions.assertEquals(response, it) }
-                .onSuccess { }
+                .onSuccess { verifier.set(it) }
                 .invoke()
         mockServer.verify()
+        verifier.verify()
     }
 }
