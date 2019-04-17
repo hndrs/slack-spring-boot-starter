bcbc320653c77237ab1aeae3e5024513292f7a74
Ben Denger <ben@kreait.com>
2019-04-17 12:12:34 +0200

    extracted MockServer Creation to HelperClass

---
+++ client/slack-spring-api-client/src/test/kotlin/io/olaph/slack/client/spring/MockServerHelper.kt
@@ -0,0 +1,37 @@
+package io.olaph.slack.client.spring
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.test.web.client.ExpectedCount
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers
+import org.springframework.test.web.client.response.MockRestResponseCreators
+import org.springframework.web.client.RestTemplate
+import java.net.URI
+
+class MockServerHelper() {
+    companion object {
+
+        fun getObjectString(response: Any): String {
+            val mapper = Jackson2ObjectMapperBuilder.json().build<ObjectMapper>()
+            return mapper.writeValueAsString(response)
+        }
+
+        /**
+         * builds a mocked RestServer that responds with the defined [responseBody] when the
+         * [methodEndpoint] is called on the [restTemplate]
+         */
+        fun buildMockRestServer(restTemplate: RestTemplate, responseBody: String, methodEndpoint: String): MockRestServiceServer {
+            val mockserver = MockRestServiceServer.createServer(restTemplate)
+            mockserver.expect(ExpectedCount.once(),
+                    MockRestRequestMatchers.requestTo(URI("https://slack.com/api/$methodEndpoint")))
+                    .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+                    .andRespond(MockRestResponseCreators.withSuccess()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(responseBody))
+            return mockserver
+        }
+    }
+}
