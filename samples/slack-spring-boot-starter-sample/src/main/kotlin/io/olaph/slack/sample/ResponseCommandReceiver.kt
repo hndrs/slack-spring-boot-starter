d9f66055169885b15547ac9cdf6185c0be04697c
Ben Denger <ben@kreait.com>
2019-04-26 12:43:32 +0200

    removed response type default value

--- samples/slack-spring-boot-starter-sample/src/main/kotlin/io/olaph/slack/sample/ResponseCommandReceiver.kt
+++ samples/slack-spring-boot-starter-sample/src/main/kotlin/io/olaph/slack/sample/ResponseCommandReceiver.kt
@@ -4,6 +4,7 @@ import io.olaph.slack.broker.receiver.SlashCommandReceiver
 import io.olaph.slack.broker.store.Team
 import io.olaph.slack.client.SlackClient
 import io.olaph.slack.dto.jackson.SlackCommand
+import io.olaph.slack.dto.jackson.group.respond.ResponseType
 import io.olaph.slack.dto.jackson.group.respond.SlackRespondMessageRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
@@ -17,7 +18,7 @@ class ResponseCommandReceiver @Autowired constructor(private val slackClient: Sl
 
     override fun onReceiveSlashCommand(slackCommand: SlackCommand, headers: HttpHeaders, team: Team) {
         this.slackClient.respond().ephemeral(slackCommand.responseUrl)
-                .with(SlackRespondMessageRequest(text = "lol"))
+                .with(SlackRespondMessageRequest(text = "lol", responseType = ResponseType.EPHEMERAL))
                 .invoke()
     }
 }
