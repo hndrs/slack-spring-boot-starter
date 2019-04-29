6b9338f3e3ff40ead066df411d3e96f69463456b
Ben Denger <ben@kreait.com>
2019-04-29 10:39:10 +0200

    added responseRestTemplate to factory

--- client/slack-spring-api-client/src/main/kotlin/io/olaph/slack/client/spring/group/respond/DefaultRespondMessageMethod.kt
+++ client/slack-spring-api-client/src/main/kotlin/io/olaph/slack/client/spring/group/respond/DefaultRespondMessageMethod.kt
@@ -3,6 +3,7 @@ package io.olaph.slack.client.spring.group.respond
 
 import io.olaph.slack.client.group.ApiCallResult
 import io.olaph.slack.client.group.respond.RespondMessageMethod
+import io.olaph.slack.client.spring.group.RestTemplateFactory
 import org.springframework.http.HttpMethod
 import org.springframework.http.RequestEntity
 import org.springframework.web.client.RestTemplate
@@ -16,14 +17,13 @@ import java.net.URI
  * @return the API Call Method containing the ResponseEntities
  */
 @Suppress("UNCHECKED_CAST")
-class DefaultRespondMessageMethod(private val responseUrl: String, private val restTemplate: RestTemplate = RestTemplate()) : RespondMessageMethod() {
+class DefaultRespondMessageMethod(private val responseUrl: String, private val restTemplate: RestTemplate = RestTemplateFactory.slackResponseTemplate()) : RespondMessageMethod() {
 
     override fun request(): ApiCallResult<Unit, Unit> {
 
         val uri = URI.create(responseUrl)
 
         val requestEntity = RequestEntity(this.params, HttpMethod.POST, uri)
-        restTemplate.errorHandler = SlackResponseErrorHandler()
         val response = restTemplate.exchange<String>(requestEntity)
         return if (response.statusCode.is2xxSuccessful) {
             this.onSuccess?.invoke(Unit)
