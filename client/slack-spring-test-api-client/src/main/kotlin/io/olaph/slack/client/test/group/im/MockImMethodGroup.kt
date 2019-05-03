247fab5e9adb6be1b9f7a07065c578dff9bda7db
Benjamin Denger <unknown>
2019-05-03 11:41:51 +0200

    added methodgroup

--- client/slack-spring-test-api-client/src/main/kotlin/io/olaph/slack/client/test/group/im/MockImMethodGroup.kt
+++ client/slack-spring-test-api-client/src/main/kotlin/io/olaph/slack/client/test/group/im/MockImMethodGroup.kt
@@ -1,11 +1,16 @@
 package io.olaph.slack.client.test.group.im
 
-import io.olaph.slack.client.group.im.*
+import io.olaph.slack.client.group.im.ImCloseMethod
+import io.olaph.slack.client.group.im.ImHistoryMethod
+import io.olaph.slack.client.group.im.ImListMethod
+import io.olaph.slack.client.group.im.ImMethodGroup
 
 class MockImMethodGroup : ImMethodGroup {
 
     private val mockImRepliesMethod = MockImRepliesMethod()
     private val mockImOpenMethod = MockImOpenMethod()
+    private val mockImMarkMethod = MockImMarkMethod()
+
 
     override fun close(authToken: String): ImCloseMethod {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -19,8 +24,8 @@ class MockImMethodGroup : ImMethodGroup {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun mark(authToken: String): ImMarkMethod {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun mark(authToken: String): MockImMarkMethod {
+        return mockImMarkMethod
     }
 
     override fun open(authToken: String): MockImOpenMethod {
