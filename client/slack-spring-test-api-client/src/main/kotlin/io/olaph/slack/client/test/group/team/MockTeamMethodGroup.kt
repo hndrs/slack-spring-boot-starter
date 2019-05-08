0ee98607b5960e22e0390af45a9b1126b7245922
Alexander Militzer <alex.militzer@kreait.com>
2019-05-08 13:56:04 +0200

    fixed method-group

--- client/slack-spring-test-api-client/src/main/kotlin/io/olaph/slack/client/test/group/team/MockTeamMethodGroup.kt
+++ client/slack-spring-test-api-client/src/main/kotlin/io/olaph/slack/client/test/group/team/MockTeamMethodGroup.kt
@@ -1,13 +1,12 @@
 package io.olaph.slack.client.test.group.team
 
-import io.olaph.slack.client.group.team.TeamGetProfileMethod
 import io.olaph.slack.client.group.team.TeamMethodGroup
 
 class MockTeamMethodGroup : TeamMethodGroup {
-    val mockTeamGetProfileMethod = MockTeamGetProfileMethod()
 
-    override fun getProfile(authToken: String): TeamGetProfileMethod {
+    private val mockTeamGetProfileMethod = MockTeamGetProfileMethod()
+
+    override fun getProfile(authToken: String): MockTeamGetProfileMethod {
         return mockTeamGetProfileMethod
     }
-
 }
