38f1c9212c53acd536bcf93554e00a261dcae86b
Ben Denger <ben@kreait.com>
2019-06-05 12:30:33 +0200

    removed newlines

--- client/slack-spring-api-client/src/main/kotlin/io/olaph/slack/client/spring/group/usergroups/DefaultUsergroupsDisableMethod.kt
+++ client/slack-spring-api-client/src/main/kotlin/io/olaph/slack/client/spring/group/usergroups/DefaultUsergroupsDisableMethod.kt
@@ -15,23 +15,18 @@ import org.springframework.web.client.RestTemplate
 class DefaultUsergroupsDisableMethod(private val authToken: String, private val restTemplate: RestTemplate = RestTemplateFactory.slackTemplate()) : UsergroupsDisableMethod() {
 
     override fun request(): ApiCallResult<SuccessfulUsergroupsDisableResponse, ErrorUsergroupsDisableResponse> {
-
         val response = SlackRequestBuilder<UsergroupsDisableResponse>(authToken, restTemplate)
                 .toMethod("usergroups.disable")
                 .returnAsType(UsergroupsDisableResponse::class.java)
                 .postWithJsonBody()
 
         return when (response.body!!) {
-
             is SuccessfulUsergroupsDisableResponse -> {
-
                 val responseEntity = response.body as SuccessfulUsergroupsDisableResponse
                 this.onSuccess?.invoke(responseEntity)
                 ApiCallResult(success = responseEntity)
             }
-
             is ErrorUsergroupsDisableResponse -> {
-
                 val responseEntity = response.body as ErrorUsergroupsDisableResponse
                 this.onFailure?.invoke(responseEntity)
                 ApiCallResult(failure = responseEntity)
