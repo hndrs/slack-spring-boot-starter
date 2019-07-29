82bb29d9ba633c68f7ba16806233d0734c9c424e
Marvin Schramm <marvin@kreait.com>
2019-07-29 17:32:30 +0200

    adds InMemoryEventStore tests

--- starter/slack-spring-boot-autoconfigure/src/main/kotlin/io/olaph/slack/broker/autoconfiguration/EvaluationReport.kt
+++ starter/slack-spring-boot-autoconfigure/src/main/kotlin/io/olaph/slack/broker/autoconfiguration/EvaluationReport.kt
@@ -4,8 +4,8 @@ import io.olaph.slack.broker.receiver.EventReceiver
 import io.olaph.slack.broker.receiver.InstallationReceiver
 import io.olaph.slack.broker.receiver.InteractiveComponentReceiver
 import io.olaph.slack.broker.receiver.MismatchCommandReciever
-import io.olaph.slack.broker.receiver.SL4JLoggingReceiver
 import io.olaph.slack.broker.receiver.SlashCommandReceiver
+import io.olaph.slack.broker.store.TeamStore
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
@@ -19,49 +19,30 @@ class EvaluationReport : ApplicationListener<ContextRefreshedEvent>, Ordered {
         println(buildEvaluationReport(event.applicationContext))
     }
 
-    fun buildEvaluationReport(applicationContext: ApplicationContext): String {
-        val eventReceiverBeans = applicationContext.getBeanNamesForType(EventReceiver::class.java)
-        val installationReceiverBeans = applicationContext.getBeanNamesForType(InstallationReceiver::class.java)
-        val interactiveCompReceiverBeans = applicationContext.getBeanNamesForType(InteractiveComponentReceiver::class.java)
-        val slashCommandReceiverBeans = applicationContext.getBeanNamesForType(SlashCommandReceiver::class.java)
-        val loggingReceiverBeans = applicationContext.getBeanNamesForType(SL4JLoggingReceiver::class.java)
-        val mismatchCommandRecieverBeans = applicationContext.getBeanNamesForType(MismatchCommandReciever::class.java)
-
+    fun buildEvaluationReport(ctx: ApplicationContext): String {
         val sb = StringBuilder()
         sb.appendln("+------------------+")
-        sb.appendln("| REGISTERED BEANS |")
+        sb.appendln("| REGISTERED SLACK BROKER COMPONENTS |")
         sb.appendln("+------------------+\n\n")
 
-        sb.appendln("Event Receiver")
-        sb.appendln("--------------\n")
-        eventReceiverBeans.forEach {
-            sb.appendln("   - $it")
-        }
-        sb.appendln("\n\nInstallation Receiver")
-        sb.appendln("---------------------\n")
-        installationReceiverBeans.forEach {
-            sb.appendln("   - $it")
-        }
-        sb.appendln("\n\nInteractive Component Receiver")
-        sb.appendln("------------------------------\n")
-        interactiveCompReceiverBeans.forEach {
-            sb.appendln("   - $it")
-        }
-        sb.appendln("\n\nSlash Command Receiver")
-        sb.appendln("----------------------\n")
-        slashCommandReceiverBeans.forEach {
-            sb.appendln("   - $it")
-        }
-        sb.appendln("\n\nLogging Receiver")
-        sb.appendln("----------------\n")
-        loggingReceiverBeans.forEach {
-            sb.appendln("   - $it")
-        }
-        sb.appendln("\n\nMismatch Command Receiver")
-        sb.appendln("-------------------------\n")
-        mismatchCommandRecieverBeans.forEach {
-            sb.appendln("   - $it")
-        }
+        this.addComponent(sb, "Event Receivers", ctx.getBeanNamesForType(EventReceiver::class.java));
+        this.addComponent(sb, "Installation Receivers", ctx.getBeanNamesForType(InstallationReceiver::class.java));
+        this.addComponent(sb, "Interactive Component Receivers", ctx.getBeanNamesForType(InteractiveComponentReceiver::class.java));
+        this.addComponent(sb, "Slash Command Receivers", ctx.getBeanNamesForType(SlashCommandReceiver::class.java));
+        this.addComponent(sb, "Mismatch Command Receivers", ctx.getBeanNamesForType(MismatchCommandReciever::class.java));
+        this.addComponent(sb, "Team Store", ctx.getBeanNamesForType(TeamStore::class.java));
+        this.addComponent(sb, "Event Store", ctx.getBeanNamesForType(TeamStore::class.java));
+
+        //TODO add default components check to report when default components are used
+
         return sb.toString()
+    }
+
+    private fun addComponent(sb: StringBuilder, title: String, names: Array<out String>) {
+        sb.appendln("\n\n$title")
+        sb.appendln("------------------------------\n")
+        names.forEach {
+            sb.appendln("   - $it")
+        }
     }
 }
