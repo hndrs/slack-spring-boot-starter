4ae7427e513a847db9d0af528b45a93d9cd5ef0b
Marvin Schramm <marvin@kreait.com>
2019-07-30 11:49:40 +0200

    adds default beans notes to evaluation report

--- starter/slack-spring-boot-autoconfigure/src/main/kotlin/io/olaph/slack/broker/autoconfiguration/EvaluationReport.kt
+++ starter/slack-spring-boot-autoconfigure/src/main/kotlin/io/olaph/slack/broker/autoconfiguration/EvaluationReport.kt
@@ -5,11 +5,15 @@ import io.olaph.slack.broker.receiver.InstallationReceiver
 import io.olaph.slack.broker.receiver.InteractiveComponentReceiver
 import io.olaph.slack.broker.receiver.MismatchCommandReciever
 import io.olaph.slack.broker.receiver.SlashCommandReceiver
+import io.olaph.slack.broker.store.EventStore
+import io.olaph.slack.broker.store.InMemoryEventStore
+import io.olaph.slack.broker.store.InMemoryTeamStore
 import io.olaph.slack.broker.store.TeamStore
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.core.Ordered
+import kotlin.reflect.KClass
 
 class EvaluationReport : ApplicationListener<ContextRefreshedEvent>, Ordered {
 
@@ -21,9 +25,9 @@ class EvaluationReport : ApplicationListener<ContextRefreshedEvent>, Ordered {
 
     fun buildEvaluationReport(ctx: ApplicationContext): String {
         val sb = StringBuilder()
-        sb.appendln("+------------------+")
+        sb.appendln("+------------------------------------+")
         sb.appendln("| REGISTERED SLACK BROKER COMPONENTS |")
-        sb.appendln("+------------------+\n\n")
+        sb.appendln("+------------------------------------+")
 
         this.addComponent(sb, "Event Receivers", ctx.getBeanNamesForType(EventReceiver::class.java));
         this.addComponent(sb, "Installation Receivers", ctx.getBeanNamesForType(InstallationReceiver::class.java));
@@ -33,14 +37,32 @@ class EvaluationReport : ApplicationListener<ContextRefreshedEvent>, Ordered {
         this.addComponent(sb, "Team Store", ctx.getBeanNamesForType(TeamStore::class.java));
         this.addComponent(sb, "Event Store", ctx.getBeanNamesForType(TeamStore::class.java));
 
+        this.defaultChecks(ctx, sb, Pair(TeamStore::class, InMemoryTeamStore::class), Pair(EventStore::class, InMemoryEventStore::class))
+
         return sb.toString()
     }
 
     private fun addComponent(sb: StringBuilder, title: String, names: Array<out String>) {
         sb.appendln("\n\n$title")
-        sb.appendln("------------------------------\n")
+        sb.appendln("------------------------------")
         names.forEach {
             sb.appendln("   - $it")
         }
+    }
+
+    private fun defaultChecks(ctx: ApplicationContext, sb: StringBuilder, vararg checks: Pair<KClass<*>, KClass<*>>) {
+        sb.appendln()
+        sb.appendln("Notes:")
+        checks.forEach {
+            try {
+                val bean = ctx.getBean(it.first.java)
+                if (it.second.isInstance(bean)) {
+                    sb.appendln("   - Default version of ${it.second.simpleName} is registered, this is not recommended for production")
+                }
+            } catch (e: Exception) {
+                //do nothing
+            }
+        }
+
     }
 }
