05b0ddaa4133153305e6c97b1288f7143d6a4831
Marvin Schramm <marvin@kreait.com>
2019-08-02 17:33:58 +0200

    cleanup

--- starter/slack-spring-boot-autoconfigure/src/main/kotlin/com/kreait/slack/broker/autoconfiguration/SlackBrokerAutoconfiguration.kt
+++ starter/slack-spring-boot-autoconfigure/src/main/kotlin/com/kreait/slack/broker/autoconfiguration/SlackBrokerAutoconfiguration.kt
@@ -8,7 +8,6 @@ import com.kreait.slack.broker.broker.CommandBroker
 import com.kreait.slack.broker.broker.EventBroker
 import com.kreait.slack.broker.broker.InstallationBroker
 import com.kreait.slack.broker.broker.InteractiveComponentBroker
-import com.kreait.slack.broker.broker.common.DisableHttpCacheInterceptor
 import com.kreait.slack.broker.configuration.EventArgumentResolver
 import com.kreait.slack.broker.configuration.InteractiveResponseArgumentResolver
 import com.kreait.slack.broker.configuration.SlackCommandArgumentResolver
@@ -43,7 +42,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableConfigurationProperties(com.kreait.slack.broker.autoconfiguration.SlackBrokerConfigurationProperties::class)
@@ -111,7 +109,7 @@ open class SlackBrokerAutoConfiguration(private val configuration: com.kreait.sl
     }
 
     @Configuration
-    open class InstallationAutoConfiguration(private val configuration: SlackBrokerConfigurationProperties) : WebMvcConfigurer {
+    open class InstallationAutoConfiguration(private val configuration: SlackBrokerConfigurationProperties) {
 
         @ConditionalOnProperty(prefix = com.kreait.slack.broker.autoconfiguration.SlackBrokerConfigurationProperties.Companion.INSTALLATION_PROPERTY_PREFIX,
                 name = ["error-redirect-url", "success-redirect-url"])
@@ -132,10 +130,6 @@ open class SlackBrokerAutoConfiguration(private val configuration: com.kreait.sl
                     slackClient,
                     InstallationBroker.Config(applicationCredentials.clientId, applicationCredentials.clientSecret, installation.successRedirectUrl, installation.errorRedirectUrl)
             )
-        }
-
-        override fun addInterceptors(registry: InterceptorRegistry) {
-            registry.addInterceptor(DisableHttpCacheInterceptor())
         }
     }
 
