6964116d91a011ae55f20dc7f02e15dd58d1c16b
Ben Denger <ben@kreait.com>
2019-08-14 10:44:28 +0200

    modified docs

--- starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/receiver/SL4JLoggingReceiver.kt
+++ starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/receiver/SL4JLoggingReceiver.kt
@@ -9,13 +9,9 @@ import org.springframework.core.Ordered
 import org.springframework.http.HttpHeaders
 
 class SL4JLoggingReceiver : EventReceiver, SlashCommandReceiver, InteractiveComponentReceiver, InstallationReceiver {
-    override fun order(): Int {
-        return Ordered.HIGHEST_PRECEDENCE
-    }
+    override fun order(): Int = Ordered.HIGHEST_PRECEDENCE
 
-    override fun shouldThrowException(exception: Exception): Boolean {
-        return false
-    }
+    override fun shouldThrowException(exception: Exception): Boolean = false
 
     override fun onReceiveInstallation(code: String, state: String, team: Team) {
         LOG.info("Received installation request")
