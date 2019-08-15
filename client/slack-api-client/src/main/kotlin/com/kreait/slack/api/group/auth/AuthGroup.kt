aac279f117c267567c2e5c4711a54e695f88667d
Ben Denger <ben@kreait.com>
2019-08-15 07:29:24 +0200

    added documentation

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/auth/AuthGroup.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/auth/AuthGroup.kt
@@ -2,9 +2,15 @@ package com.kreait.slack.api.group.auth
 
 interface AuthGroup {
 
-    //TODO DOC
+    /**
+     * Checks authentication & identity.
+     * https://api.slack.com/methods/auth.test
+     */
     fun test(authToken: String): AuthTestMethod
 
-    //TODO DOC
+    /**
+     * Revokes a token.
+     * https://api.slack.com/methods/auth.revoke
+     */
     fun revoke(authToken: String): AuthRevokeMethod
 }
