43b57783cd44b821a470afef581c59f976f302db
Marvin Schramm <marvin@kreait.com>
2019-08-15 17:18:44 +0200

    add tests for Instant and String Time Extensions

--- data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/composition/TextExtensions.kt
+++ data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/composition/TextExtensions.kt
@@ -1,7 +1,5 @@
 package com.kreait.slack.api.contract.jackson.common.messaging.composition
 
-import com.kreait.slack.api.contract.jackson.common.messaging.composition.Text
-
 
 fun Text.Companion.sample(): Text = Text(Text.Type.MARKDOWN, "")
 fun Text.Companion.plainTextSample(): Text = Text(Text.Type.PLAIN_TEXT, "")
