fdb68abb4b5840932ac5f7d3b5d3f1cad8db0bbe
Marvin Schramm <marvin@kreait.com>
2019-08-17 01:15:49 +0200

    fix tests

--- data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/types/GroupExtension.kt
+++ data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/types/GroupExtension.kt
@@ -1,14 +1,8 @@
 package com.kreait.slack.api.contract.jackson.common.types
 
+import com.kreait.slack.api.contract.jackson.common.InstantSample
+
 fun Group.Companion.sample(): Group {
-    return Group(0, "", "", false, true, false, "", "",
+    return Group(InstantSample.sample(), "", "", false, true, false, InstantSample.sample(), "",
             listOf(), "", Purpose.sample(), Topic.sample(), 0, 0)
-}
-
-fun Purpose.Companion.sample(): Purpose {
-    return Purpose("", 0, "")
-}
-
-fun Topic.Companion.sample(): Topic {
-    return Topic("", 0, "")
 }
