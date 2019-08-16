6525f431ffb1fa0c742718ccffd9b3a1f50afcb3
Ben Denger <ben@kreait.com>
2019-08-16 10:51:14 +0200

    moved group dto to common

--- data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/groups/Create.kt
+++ data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/groups/Create.kt
@@ -1,24 +1,14 @@
 package com.kreait.slack.api.contract.jackson.group.groups
 
+import com.kreait.slack.api.contract.jackson.common.types.Group
+import com.kreait.slack.api.contract.jackson.common.types.sample
+
 fun GroupsCreateRequest.Companion.sample(): GroupsCreateRequest {
     return GroupsCreateRequest("")
 }
 
 fun SuccessfulGroupsCreateResponse.Companion.sample(): SuccessfulGroupsCreateResponse {
     return SuccessfulGroupsCreateResponse(true, Group.sample())
-}
-
-fun Group.Companion.sample(): Group {
-    return Group(0, "", "", false, true, false, "", "",
-            listOf(), "", Purpose.sample(), Topic.sample(), 0, 0)
-}
-
-fun Purpose.Companion.sample(): Purpose {
-    return Purpose("", 0, "")
-}
-
-fun Topic.Companion.sample(): Topic {
-    return Topic("", 0, "")
 }
 
 fun ErrorGroupsCreateResponse.Companion.sample(): ErrorGroupsCreateResponse {
