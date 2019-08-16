4e93db063ad9ffbc9b3b0a173b21594a1029386e
Ben Denger <ben@kreait.com>
2019-08-16 08:42:40 +0200

    added groups create method

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/groups/GroupsCreateMethod.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/groups/GroupsCreateMethod.kt
@@ -1,3 +1,8 @@
 package com.kreait.slack.api.group.groups
 
-abstract class GroupsCreateMethod
+import com.kreait.slack.api.contract.jackson.group.groups.ErrorGroupsCreateResponse
+import com.kreait.slack.api.contract.jackson.group.groups.GroupsCreateRequest
+import com.kreait.slack.api.contract.jackson.group.groups.SuccessfulGroupsCreateResponse
+import com.kreait.slack.api.group.ApiCallMethod
+
+abstract class GroupsCreateMethod : ApiCallMethod<GroupsCreateMethod, SuccessfulGroupsCreateResponse, ErrorGroupsCreateResponse, GroupsCreateRequest>()
