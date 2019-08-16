6588f8b43d2c7d6a0dbec7629000a4e6650cf91c
Ben Denger <ben@kreait.com>
2019-08-16 10:51:14 +0200

    moved group dto to common

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/groups/Create.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/groups/Create.kt
@@ -3,7 +3,8 @@ package com.kreait.slack.api.contract.jackson.group.groups
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.kreait.slack.api.contract.jackson.JacksonDataClass
+import com.kreait.slack.api.contract.jackson.common.types.Group
+import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
         include = JsonTypeInfo.As.PROPERTY,
@@ -41,54 +42,5 @@ data class ErrorGroupsCreateResponse constructor(
  */
 data class GroupsCreateRequest(@JsonProperty("name") val name: String,
                                @JsonProperty("validate") val validate: Boolean = true) {
-    companion object
-}
-
-data class Group(
-        @JsonProperty("created")
-        val created: Int?,
-        @JsonProperty("creator")
-        val creator: String?,
-        @JsonProperty("id")
-        val id: String?,
-        @JsonProperty("is_archived")
-        val isArchived: Boolean,
-        @JsonProperty("is_group")
-        val isGroup: Boolean = true,
-        @JsonProperty("is_open")
-        val isOpen: Boolean,
-        @JsonProperty("last_read")
-        val lastRead: String?,
-        @JsonProperty("latest")
-        val latest: Any?,
-        @JsonProperty("members")
-        val members: List<String?>?,
-        @JsonProperty("name")
-        val name: String?,
-        @JsonProperty("purpose")
-        val purpose: Purpose?,
-        @JsonProperty("topic")
-        val topic: Topic?,
-        @JsonProperty("unread_count")
-        val unreadCount: Int?,
-        @JsonProperty("unread_count_display")
-        val unreadCountDisplay: Int?
-) {
-    companion object
-}
-
-data class Topic(
-        @JsonProperty("creator") val creator: String?,
-        @JsonProperty("last_set") val lastSet: Int?,
-        @JsonProperty("value") val value: String?
-) {
-    companion object
-}
-
-data class Purpose(
-        @JsonProperty("creator") val creator: String?,
-        @JsonProperty("last_set") val lastSet: Int?,
-        @JsonProperty("value") val value: String?
-) {
     companion object
 }
