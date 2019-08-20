9e84a09ca09176894b3ff20d3b4e9799d8b46a5b
Ben Denger <ben@kreait.com>
2019-08-20 17:14:31 +0200

    WIP: implemented replies method

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/groups/GroupsRepliesMethod.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/groups/GroupsRepliesMethod.kt
@@ -1,8 +1,11 @@
 package com.kreait.slack.api.group.groups
 
 import com.kreait.slack.api.contract.jackson.group.groups.ErrorGroupsArchiveResponse
+import com.kreait.slack.api.contract.jackson.group.groups.ErrorGroupsCreateResponse
 import com.kreait.slack.api.contract.jackson.group.groups.GroupsArchiveRequest
+import com.kreait.slack.api.contract.jackson.group.groups.GroupsCreateRequest
 import com.kreait.slack.api.contract.jackson.group.groups.SuccessfulGroupsArchiveResponse
+import com.kreait.slack.api.contract.jackson.group.groups.SuccessfulGroupsCreateResponse
 import com.kreait.slack.api.group.ApiCallMethod
 
-abstract class GroupsRepliesMethod
+abstract class GroupsRepliesMethod: ApiCallMethod<GroupsCreateMethod, SuccessfulGroupsCreateResponse, ErrorGroupsCreateResponse, GroupsCreateRequest>()
