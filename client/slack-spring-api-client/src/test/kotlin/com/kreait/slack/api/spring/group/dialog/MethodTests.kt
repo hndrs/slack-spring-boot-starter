6ff116c4c94742afde7f76137307789678b9ff1a
Marvin Schramm <marvin@kreait.com>
2019-08-21 14:32:14 +0200

    dialog test factory

---
+++ client/slack-spring-api-client/src/test/kotlin/com/kreait/slack/api/spring/group/dialog/MethodTests.kt
@@ -0,0 +1,40 @@
+package com.kreait.slack.api.spring.group.dialog
+
+import com.kreait.slack.api.contract.jackson.group.chat.ChatDeleteRequest
+import com.kreait.slack.api.contract.jackson.group.chat.sample
+import com.kreait.slack.api.contract.jackson.group.dialog.ErrorOpenDialogResponse
+import com.kreait.slack.api.contract.jackson.group.dialog.SuccessfulOpenDialogResponse
+import com.kreait.slack.api.contract.jackson.group.dialog.sample
+import com.kreait.slack.api.spring.DynamicGroupTests
+import com.kreait.slack.api.spring.MetaInfo
+import com.kreait.slack.api.spring.group.RestTemplateFactory
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.springframework.web.client.RestTemplate
+
+
+@DisplayName("Dialog MethodGroup")
+class MethodInvocationTest {
+
+    protected lateinit var mockTemplate: RestTemplate
+
+
+    @BeforeEach
+    fun setup() {
+        mockTemplate = RestTemplateFactory.slackTemplate()
+    }
+
+    @TestFactory
+    @DisplayName("Method Invocation Tests")
+    fun methodInvocations(): List<DynamicTest> {
+        return DynamicGroupTests.methodInvocations(testCases(), mockTemplate)
+    }
+
+    private fun testCases() = listOf(
+            MetaInfo("dialog.open", SuccessfulOpenDialogResponse.sample(), ErrorOpenDialogResponse.sample(), ChatDeleteRequest.sample(), DefaultDialogOpenMethod("", mockTemplate))
+    )
+
+
+}
