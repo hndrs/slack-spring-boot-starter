58f7f74c61fc0eb62c2aaf868b92621447158f20
Ben Denger <ben@kreait.com>
2019-08-23 14:22:48 +0200

    modified reminder

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/reminders/common/Reminder.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/reminders/common/Reminder.kt
@@ -1,15 +1,17 @@
 package com.kreait.slack.api.contract.jackson.group.reminders.common
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.kreait.slack.api.contract.jackson.util.InstantToInt
+import java.time.Instant
 
 data class Reminder(
-        @JsonProperty("complete_ts") val completeTimestamp: Int?,
-        @JsonProperty("creator") val creator: String?,
-        @JsonProperty("id") val id: String?,
-        @JsonProperty("recurring") val recurring: Boolean?,
-        @JsonProperty("text") val text: String?,
-        @JsonProperty("time") val time: Int?,
-        @JsonProperty("user") val userId: String?
+        @InstantToInt @JsonProperty("complete_ts") val completeTimestamp: Instant?,
+        @JsonProperty("creator") val createdBy: String,
+        @JsonProperty("id") val id: String,
+        @JsonProperty("recurring") val recurring: Boolean,
+        @JsonProperty("text") val text: String,
+        @InstantToInt @JsonProperty("time") val time: Instant?,
+        @JsonProperty("user") val userId: String
 ) {
     companion object
 }
