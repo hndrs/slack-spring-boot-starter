ceea6b16c7148d0f0299d805148ac85b077ad423
Ben Denger <ben@kreait.com>
2019-08-26 07:58:46 +0200

    added dialog tests

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/dialog/Open.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/dialog/Open.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 
-
+//TODO: Refactor names, e.g. should be DialogOpenResponse instead of SlackOpenDialogResponse
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
         include = JsonTypeInfo.As.PROPERTY,
         property = "ok",
