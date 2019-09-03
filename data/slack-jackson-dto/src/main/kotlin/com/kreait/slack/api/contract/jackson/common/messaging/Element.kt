5f06381ba752c2f41202fa5f2ac556e631e403b9
Ben Denger <ben@kreait.com>
2019-09-03 07:56:34 +0200

    refactored blocks & element dtos

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/Element.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/Element.kt
@@ -1,6 +1,8 @@
 package com.kreait.slack.api.contract.jackson.common.messaging
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
@@ -13,13 +15,30 @@ import com.kreait.slack.api.contract.jackson.common.messaging.composition.Confir
 import com.kreait.slack.api.contract.jackson.common.messaging.composition.Option
 import com.kreait.slack.api.contract.jackson.common.messaging.composition.OptionGroup
 import com.kreait.slack.api.contract.jackson.common.messaging.composition.Text
+import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 import java.time.LocalDate
 
-sealed class Element(@JsonProperty("type") val type: Type) {
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type",
+        visible = true)
+@JsonSubTypes(
+        JsonSubTypes.Type(value = Element.Image::class, name = "image"),
+        JsonSubTypes.Type(value = Element.Button::class, name = "button"),
+        JsonSubTypes.Type(value = Element.StaticSelect::class, name = "static_select"),
+        JsonSubTypes.Type(value = Element.ExternalSelect::class, name = "external_select"),
+        JsonSubTypes.Type(value = Element.UsersSelect::class, name = "users_select"),
+        JsonSubTypes.Type(value = Element.ConversationsSelect::class, name = "conversations_select"),
+        JsonSubTypes.Type(value = Element.ChannelsSelect::class, name = "channels_select"),
+        JsonSubTypes.Type(value = Element.DatePicker::class, name = "datepicker"),
+        JsonSubTypes.Type(value = Element.Overflow::class, name = "overflow")
+)
+@JacksonDataClass
+sealed class Element(@JsonProperty("type") open val type: Type) {
 
     @JsonSerialize(using = Type.Serializer::class)
     @JsonDeserialize(using = Type.Deserializer::class)
-    enum class Type(private val typeString: String) {
+    enum class Type(internal val typeString: String) {
         IMAGE("image"),
         BUTTON("button"),
         STATIC_SELECT("static_select"),
@@ -32,16 +51,14 @@ sealed class Element(@JsonProperty("type") val type: Type) {
 
         class Serializer : JsonSerializer<Type>() {
             override fun serialize(value: Type, gen: JsonGenerator?, serializers: SerializerProvider?) {
-                gen?.writeString(value.typeString)
+                gen?.writeString(value.typeString.toLowerCase())
             }
         }
 
         class Deserializer : JsonDeserializer<Type>() {
             override fun deserialize(p: JsonParser, ctxt: DeserializationContext?): Type {
-                println(p.text)
-                return BUTTON
+                return valueOf(p.text.toUpperCase())
             }
-
         }
     }
 
@@ -49,6 +66,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#image
      */
+    @JacksonDataClass
     data class Image constructor(@JsonProperty("image_url") val imageUrl: String,
                                  @JsonProperty("alt_text") val altText: String) : Element(Type.IMAGE) {
         companion object
@@ -57,6 +75,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#button
      */
+    @JacksonDataClass
     data class Button constructor(@JsonProperty("text") val text: Text,
                                   @JsonProperty("action_id") val actionId: String,
                                   @JsonProperty("url") val url: String? = null,
@@ -68,6 +87,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#select
      */
+    @JacksonDataClass
     data class StaticSelect constructor(@JsonProperty("placeholder") val placeholderText: Text,
                                         @JsonProperty("action_id") val actionId: String,
                                         @JsonProperty("options") val options: List<Option>,
@@ -81,6 +101,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#external-select
      */
+    @JacksonDataClass
     data class ExternalSelect constructor(@JsonProperty("placeholder") val placeholderText: Text,
                                           @JsonProperty("action_id") val actionId: String,
                                           @JsonProperty("options") val options: List<Option>,
@@ -94,6 +115,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#users-select
      */
+    @JacksonDataClass
     data class UsersSelect constructor(@JsonProperty("placeholder") val placeholderText: Text,
                                        @JsonProperty("action_id") val actionId: String,
                                        @JsonProperty("initial_user") val initialUserId: String? = null,
@@ -104,6 +126,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#conversation-select
      */
+    @JacksonDataClass
     data class ConversationsSelect constructor(@JsonProperty("placeholder") val placeholderText: Text,
                                                @JsonProperty("action_id") val actionId: String,
                                                @JsonProperty("initial_conversation") val initialConversationId: String? = null,
@@ -114,6 +137,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#channel-select
      */
+    @JacksonDataClass
     data class ChannelsSelect constructor(@JsonProperty("placeholder") val placeholderText: Text,
                                           @JsonProperty("action_id") val actionId: String,
                                           @JsonProperty("initial_channels") val initialChannelsId: String? = null,
@@ -124,6 +148,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#overflow
      */
+    @JacksonDataClass
     data class Overflow constructor(@JsonProperty("action_id") val actionId: String,
                                     @JsonProperty("options") val options: List<Option>,
                                     @JsonProperty("confirm") val confirmation: Confirmation? = null) : Element(Type.OVERFLOW) {
@@ -133,6 +158,7 @@ sealed class Element(@JsonProperty("type") val type: Type) {
     /**
      * https://api.slack.com/reference/messaging/block-elements#datepicker
      */
+    @JacksonDataClass
     data class DatePicker constructor(@JsonProperty("action_id") val actionId: String,
                                       @JsonProperty("placeholder") val placeholderText: Text? = null,
                                       @JsonProperty("initial_date") val initialChannelsId: LocalDate? = null,
