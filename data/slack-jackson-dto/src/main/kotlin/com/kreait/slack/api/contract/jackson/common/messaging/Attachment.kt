af8751b971478c28092d17c41534c37db042f8fb
Alexander Militzer <alex.militzer@kreait.com>
2019-09-04 16:20:44 +0200

    added kdoc for messaging dtos

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/Attachment.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/messaging/Attachment.kt
@@ -12,6 +12,21 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 import com.kreait.slack.api.contract.jackson.common.Action
 
+/**
+ * Attachments are used to add secondary content to messages.
+ *
+ * @property title Title text that appears at the top of the attachment
+ * @property fallback A fallback summary in plain text for clients that don't show formatted texts (IRC, Mobile Notifications, etc.)
+ * @property pretext Text that appears above the attachment block.
+ * @property color The color of the left border of the attachment defined by the [Color] object.
+ * @property attachmentType Indicates the type of the attachment.
+ * @property callbackId A callback reference to that attachment.
+ * @property actions [Action]s that can be used within an attachment, such as Buttons and Selectors.
+ * @property text The main body text of the attachment.
+ * @property authorName The name of the author that is displayed at the attachment.
+ * @see [Action]
+ * @see [Slack API Documentation](https://api.slack.com/reference/messaging/attachments)
+ */
 @JacksonDataClass
 data class Attachment(
         @JsonProperty("title") val title: String? = null,
@@ -23,20 +38,31 @@ data class Attachment(
         @JsonProperty("actions") val actions: List<Action>? = listOf(),
         @JsonProperty("text") val text: String? = null,
         @JsonProperty("author_name") val authorName: String? = null) {
-
     companion object
 }
 
+/**
+ * A helper data-class for the standard-colors used in Slack
+ *
+ * @property code Choses one of the standard colors depending on the identifier.
+ * @see [Slack API Documentation](https://api.slack.com/docs/message-guidelines)
+ */
 @JsonSerialize(using = Color.Serializer::class)
 @JsonDeserialize(using = Color.Deserializer::class)
 @JacksonDataClass
 data class Color(val code: String?) {
 
+    /**
+     * @property GOOD Translates to a green color tone.
+     * @property WARNING Translates to a orange color tone.
+     * @property DANGER Translates to a red color tone.
+     * @property NEUTRAL Translates to a grey tone.
+     */
     companion object {
-        val GOOD = Color(code = "good")
-        val WARNING = Color(code = "warning")
-        val DANGER = Color(code = "danger")
-        val NEUTRAL = Color(code = "neutral")
+        private val GOOD = Color(code = "good")
+        private val WARNING = Color(code = "warning")
+        private val DANGER = Color(code = "danger")
+        private val NEUTRAL = Color(code = "neutral")
 
         fun ofHex(hexCode: String): Color {
             //TODO validation
@@ -57,6 +83,19 @@ data class Color(val code: String?) {
     }
 }
 
+/**
+ * Data class with the same properties as [Attachment] but used to update predecessing attachments.
+ *
+ * @property pretext Text that appears above the attachment block.
+ * @property text The main body text of the attachment.
+ * @property title Title text that appears at the top of the attachment.
+ * @property color The color of the left border of the attachment.
+ * @property attachmentType Indicates the type of the attachment.
+ * @property callbackId A callback reference to that attachment.
+ * @property actions [Action]s that can be used within an attachment, such as Buttons and Selectors.
+ * @property authorName The name of the author that is displayed at the attachment.
+ * @property fallback A fallback summary in plain text for clients that don't show formatted texts (IRC, Mobile Notifications, etc.)
+ */
 @JacksonDataClass
 data class UpdateAttachment(
         @JsonProperty("pretext") val pretext: String? = null,
