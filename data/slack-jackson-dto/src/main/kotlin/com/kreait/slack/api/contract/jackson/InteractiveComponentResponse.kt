5e684691d7b6f2153c291cd92f899cd48e6ae16d
Ben Denger <ben@kreait.com>
2019-09-04 10:35:38 +0200

    made type of InteractiveComponentResponse generic

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/InteractiveComponentResponse.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/InteractiveComponentResponse.kt
@@ -1,23 +1,62 @@
 package com.kreait.slack.api.contract.jackson
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.kreait.slack.api.contract.jackson.common.Action
 import com.kreait.slack.api.contract.jackson.common.messaging.Block
+import com.kreait.slack.api.contract.jackson.common.messaging.composition.Text
 import com.kreait.slack.api.contract.jackson.group.chat.Message
 import com.kreait.slack.api.contract.jackson.util.InstantToString
 import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 import java.time.Instant
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type",
+        visible = true)
+@JsonSubTypes(
+        JsonSubTypes.Type(value = InteractiveMessage::class, name = "interactive_message"),
+        JsonSubTypes.Type(value = BlockActions::class, name = "block_actions"))
 @JacksonDataClass
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class InteractiveComponentResponse(
-        //TODO can this really be optional?
-        @JsonProperty("type") val type: String?,
+abstract class InteractiveComponentResponse(
+        @JsonProperty("type") val type: Type?,
+        @JsonProperty("team") open val team: Team) {
+
+    @JsonSerialize(using = Type.Serializer::class)
+    @JsonDeserialize(using = Type.Deserializer::class)
+    enum class Type(internal val typeString: String) {
+        INTERACTIVE_MESSAGE("interactive_message"),
+        BLOCK_ACTIONS("block_actions");
+
+        class Serializer : JsonSerializer<Type>() {
+            override fun serialize(value: Type?, gen: JsonGenerator?, provider: SerializerProvider?) {
+                gen?.writeString(value.toString().toLowerCase())
+            }
+        }
+
+        class Deserializer : JsonDeserializer<Type>() {
+            override fun deserialize(p: JsonParser, ctxt: DeserializationContext?): Type {
+                return valueOf(p.text.toUpperCase())
+            }
+        }
+    }
+}
+
+@JacksonDataClass
+data class InteractiveMessage(
+        @JsonProperty("team") override val team: Team,
         @JsonProperty("token") val token: String?,
         @InstantToString @JsonProperty("action_ts") val actionTimestamp: Instant?,
         @InstantToString @JsonProperty("message_ts") val timestamp: Instant?,
-        @JsonProperty("team") val team: Team,
         @JsonProperty("user") val user: User,
         @JsonProperty("state") val state: String? = "",
         @JsonProperty("channel") val channel: Channel,
@@ -28,43 +67,58 @@ data class InteractiveComponentResponse(
         @JsonProperty("blocks") val blocks: List<Block>? = listOf(),
         @JsonProperty("container") val container: Container? = null,
         @JsonProperty("trigger_id") val triggerId: String?,
-        @JsonProperty("message") val message: Message?) {
-
+        @JsonProperty("message") val message: Message?) : InteractiveComponentResponse(Type.INTERACTIVE_MESSAGE, team) {
     companion object
-
-    @JacksonDataClass
-    data class Channel(@JsonProperty("id") val id: String,
-                       @JsonProperty("name") val name: String) {
-        companion object
-    }
-
-    @JacksonDataClass
-    data class User(@JsonProperty("id") val id: String,
-                    @JsonProperty("name") val name: String,
-                    @JsonProperty("username") val username: String? = null,
-            //TODO can this really be optional?
-                    @JsonProperty("team_id") val teamId: String?) {
-        companion object
-    }
-
-    @JacksonDataClass
-    data class Team(@JsonProperty("id") val id: String,
-                    @JsonProperty("domain") val domain: String,
-                    @JsonProperty("enterprise_id") val enterpriseId: String?,
-                    @JsonProperty("enterprise_name") val enterpriseName: String?) {
-        companion object
-    }
-
-    data class Container(
-            @JsonProperty("channel_id") val channelId: String?,
-            @JsonProperty("is_ephemeral") val isEphemeral: Boolean?,
-            @JsonProperty("message_ts") val messageTs: String?,
-            @JsonProperty("type") val type: String?
-    )
 }
 
+@JacksonDataClass
+data class BlockActions(@JsonProperty("team") override val team: Team,
+                        @JsonProperty("user") val user: User,
+                        @JsonProperty("token") val token: String?,
+                        @JsonProperty("api_app_id") val apiAppId: String?,
+                        @JsonProperty("container") val container: Container? = null,
+                        @JsonProperty("trigger_id") val triggerId: String?,
+                        @JsonProperty("channel") val channel: Channel,
+                        @JsonProperty("response_url") val responseUrl: String?,
+                        @JsonProperty("actions") val actions: List<BlockAction>? = listOf()) : InteractiveComponentResponse(Type.BLOCK_ACTIONS, team)
+
+@JacksonDataClass
+data class Channel(@JsonProperty("id") val id: String,
+                   @JsonProperty("name") val name: String) {
+    companion object
+}
+
+@JacksonDataClass
+data class User(@JsonProperty("id") val id: String,
+                @JsonProperty("name") val name: String,
+                @JsonProperty("username") val username: String? = null,
+        //TODO can this really be optional?
+                @JsonProperty("team_id") val teamId: String?) {
+    companion object
+}
+
+@JacksonDataClass
+data class Team(@JsonProperty("id") val id: String,
+                @JsonProperty("domain") val domain: String,
+                @JsonProperty("enterprise_id") val enterpriseId: String?,
+                @JsonProperty("enterprise_name") val enterpriseName: String?) {
+    companion object
+}
+
+data class Container(
+        @JsonProperty("channel_id") val channelId: String?,
+        @JsonProperty("is_ephemeral") val isEphemeral: Boolean?,
+        @JsonProperty("message_ts") val messageTs: String?,
+        @JsonProperty("type") val type: String?
+)
 
 
-
-
+@JacksonDataClass
+data class BlockAction(
+        @JsonProperty("action_id") val actionId: String?,
+        @JsonProperty("action_ts") val actionTs: String?,
+        @JsonProperty("block_id") val blockId: String?,
+        @JsonProperty("text") val text: Text?,
+        @JsonProperty("type") val type: String?
+)
 
