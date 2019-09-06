938e441b6943863fdaf0c9059d3d97706f279159
Ben Denger <ben@kreait.com>
2019-09-06 10:48:38 +0200

    added kdoc

--- data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/types/UserProfile.kt
+++ data/slack-jackson-dto/src/main/kotlin/com/kreait/slack/api/contract/jackson/common/types/UserProfile.kt
@@ -3,6 +3,36 @@ package com.kreait.slack.api.contract.jackson.common.types
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.kreait.slack.api.contract.jackson.util.JacksonDataClass
 
+/**
+ * The Profile of a User
+ *
+ * @property title title of a user
+ * @property phone phone number
+ * @property skype skype address
+ * @property realName the real name with escaped special characters
+ * @property realNameNormalized the real name with normalized characters
+ * @property displayName the display name with escaped special characters
+ * @property displayNameNormalized the display name with normalized characters
+ * @property fields
+ * @property statusText the status text
+ * @property statusEmoji the status emoji
+ * @property statusExpiration the expiration date of the status
+ * @property avatarHash the hash of the avatar
+ * @property alwaysActive
+ * @property image_original url to original image
+ * @property email email address of the user
+ * @property firstName  first name of the user
+ * @property lastName last name of the user
+ * @property image24 url to scaled image
+ * @property image32 url to scaled image
+ * @property image48 url to scaled image
+ * @property image72 url to scaled image
+ * @property image192 url to scaled image
+ * @property image512 url to scaled image
+ * @property image_1024 url to scaled image
+ * @property statusTextCanonical
+ * @property team the team-id of the user
+ */
 @JacksonDataClass
 data class UserProfile(
         @JsonProperty("title") val title: String?,
