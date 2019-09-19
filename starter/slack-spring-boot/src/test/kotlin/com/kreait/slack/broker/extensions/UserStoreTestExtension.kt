6be23aaf4d85b792b175bdcafe8c6ab33c71066d
Ben Denger <ben@kreait.com>
2019-09-19 16:18:50 +0200

    modified tests

--- starter/slack-spring-boot/src/test/kotlin/com/kreait/slack/broker/extensions/UserStoreTestExtension.kt
+++ starter/slack-spring-boot/src/test/kotlin/com/kreait/slack/broker/extensions/UserStoreTestExtension.kt
@@ -5,6 +5,6 @@ import com.kreait.slack.broker.store.user.User
 
 fun User.Companion.sample(): User {
     return User("testId", "team", "", false, "", "", "", "", 0,
-            User.UserProfile("", "", "", "", "", "test", "", mapOf(), "", "", 0, "", false, "", "", "", "", "", "", "", "", "", "", "", "", ""),
+            User.UserProfile("", "", "", "", "", "", "", mapOf(), "", "", 0, "", false, "", "", "", "", "", "", "", "", "", "", "", "", ""),
             isAdmin = false, isOwner = false, isPrimaryOwner = false, isRestricted = false, isUltraRestricted = false, isBot = false, lastModifiedAt = InstantSample.sample(), isAppUser = false, has2fa = false, locale = "")
 }
