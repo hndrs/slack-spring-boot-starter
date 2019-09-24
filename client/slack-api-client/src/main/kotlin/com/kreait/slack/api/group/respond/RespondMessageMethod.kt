35a254d7ae9c31d1621dfe1f03fcaa8c9945ca0f
Marvin Schramm <marvin@kreait.com>
2019-09-24 13:57:29 +0200

    add respond docs

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/respond/RespondMessageMethod.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/respond/RespondMessageMethod.kt
@@ -3,6 +3,11 @@ package com.kreait.slack.api.group.respond
 import com.kreait.slack.api.contract.jackson.group.respond.RespondMessageRequest
 import com.kreait.slack.api.group.ApiCallMethod
 
+/**
+ * Abstract representation of an slack api operation
+ * to respond to a request
+ * https://api.slack.com/interactive-messages
+ */
 abstract class RespondMessageMethod : ApiCallMethod<RespondMessageMethod, Unit, Unit, RespondMessageRequest>()
 
 
