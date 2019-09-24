68a916957a064e0b99e7650bae6e4fcbc5819cbf
Marvin Schramm <marvin@kreait.com>
2019-09-24 13:59:37 +0200

    add team docs

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/team/TeamGetProfileMethod.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/team/TeamGetProfileMethod.kt
@@ -1,8 +1,12 @@
 package com.kreait.slack.api.group.team
 
 import com.kreait.slack.api.contract.jackson.group.team.ErrorProfileResponse
-import com.kreait.slack.api.contract.jackson.group.team.SuccessfulProfileResponse
 import com.kreait.slack.api.contract.jackson.group.team.ProfileRequest
+import com.kreait.slack.api.contract.jackson.group.team.SuccessfulProfileResponse
 import com.kreait.slack.api.group.ApiCallMethod
 
+/**
+ * Abstract representation of an slack api operation
+ * https://api.slack.com/methods/team.profile.get
+ */
 abstract class TeamGetProfileMethod : ApiCallMethod<TeamGetProfileMethod, SuccessfulProfileResponse, ErrorProfileResponse, ProfileRequest>()
