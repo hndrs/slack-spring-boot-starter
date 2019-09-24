ffd977e89a9857cb20a899f47835da568219e674
Marvin Schramm <marvin@kreait.com>
2019-09-24 13:57:29 +0200

    add respond docs

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/respond/RespondMethodGroup.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/respond/RespondMethodGroup.kt
@@ -1,5 +1,10 @@
 package com.kreait.slack.api.group.respond
 
+/**
+ * Convenience class to handle the respond operations
+ *
+ *  [Slack Api Documentation](https://api.slack.com/methods)
+ */
 interface RespondMethodGroup {
 
     /**
