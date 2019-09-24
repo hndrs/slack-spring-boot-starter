16170dae913b0550cefa94939c4f61203c65e3e2
Marvin Schramm <marvin@kreait.com>
2019-09-24 13:31:21 +0200

    add oauth docs

--- client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/oauth/OauthAccessMethod.kt
+++ client/slack-api-client/src/main/kotlin/com/kreait/slack/api/group/oauth/OauthAccessMethod.kt
@@ -5,4 +5,8 @@ import com.kreait.slack.api.contract.jackson.group.oauth.AccessRequest
 import com.kreait.slack.api.contract.jackson.group.oauth.SuccessfullAccessResponse
 import com.kreait.slack.api.group.ApiCallMethod
 
+/**
+ * Abstract representation of an slack api operation
+ * https://api.slack.com/methods/oauth.access
+ */
 abstract class OauthAccessMethod : ApiCallMethod<OauthAccessMethod, SuccessfullAccessResponse, ErrorAccessResponse, AccessRequest>()
