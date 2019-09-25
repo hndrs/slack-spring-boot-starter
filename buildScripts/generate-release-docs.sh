3df114e111c294ba89eab52b61a802d5d3f9623f
Marvin Schramm <marvin@kreait.com>
2019-09-25 10:42:16 +0200

    test generate snapshot build script

---
+++ buildScripts/generate-release-docs.sh
@@ -0,0 +1,11 @@
+./gradlew clean dokka asciidoctor
+mkdir -p build/docs/latest/reference
+mkdir -p build/docs/$(cat version.txt)/reference
+
+cp -R build/generated-docs/api build/docs/latest/api
+cp -R build/generated-docs/api build/docs/$(cat version.txt)-SNAPSHOT/api
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/$(cat version.txt)/reference
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/latest/reference
+
+
+
