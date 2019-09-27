2d5763e859cc01ba35d9ac1ae8181ab13753740d
Ben Denger <ben@kreait.com>
2019-09-27 07:58:26 +0200

    Modified scripts

--- buildScripts/generate-release-docs.sh
+++ buildScripts/generate-release-docs.sh
@@ -4,8 +4,8 @@ mkdir -p build/docs/$(cat version.txt)/reference
 
 cp -R build/generated-docs/api build/docs/latest/api
 cp -R build/generated-docs/api build/docs/$(cat version.txt)-SNAPSHOT/api
-cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/$(cat version.txt)/reference
-cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/latest/reference
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5/ build/docs/$(cat version.txt)/reference
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5/ build/docs/latest/reference
 
 
 
--- buildScripts/generate-snapshot-docs.sh
+++ buildScripts/generate-snapshot-docs.sh
@@ -4,8 +4,8 @@ mkdir -p build/docs/$(cat version.txt)-SNAPSHOT/reference
 
 cp -R build/generated-docs/api build/docs/latest-SNAPSHOT/api
 cp -R build/generated-docs/api build/docs/$(cat version.txt)-SNAPSHOT/api
-cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/$(cat version.txt)-SNAPSHOT/reference
-cp -R docs/slack-spring-boot-docs/build/generated-docs/html5 build/docs/latest-SNAPSHOT/reference
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5/ build/docs/$(cat version.txt)-SNAPSHOT/reference
+cp -R docs/slack-spring-boot-docs/build/generated-docs/html5/ build/docs/latest-SNAPSHOT/reference
 
 
 
