2d6c89dad8a9f20a26bd77da24849d13f77bb558
Volodymyr Abaza <vabaza@cloudlinux.com>
2019-10-25 14:28:03 +0300

    add to contributors list

--- publish-meta.gradle.kts
+++ publish-meta.gradle.kts
@@ -9,7 +9,10 @@ extra["developers"] = listOf(
         Developer("bendenger", "Ben Denger", "ben.denger.bd@gmail.com"),
         Developer("andakawa", "Alexander Militzer", "alex.militzer@gmail.com")
 )
-extra["contributors"] = listOf(Contributor("Jascha Ostholt", "jascha.ostholt@gmail.com"))
+extra["contributors"] = listOf(
+    Contributor("Jascha Ostholt", "jascha.ostholt@gmail.com"),
+    Contributor("Volodymyr Abaza", "abaza.org@gmail.com")
+)
 extra["organization"] = Organization("kreait GmbH", "https://kreait.com")
 extra["scm"] = Scm("scm:git:git://github.com/kreait/spring-boot-starter.git", "https://github.com/kreait/slack-spring-boot-starter")
 extra["license"] = License("http://www.opensource.org/licenses/mit-license.php", "MIT License")
