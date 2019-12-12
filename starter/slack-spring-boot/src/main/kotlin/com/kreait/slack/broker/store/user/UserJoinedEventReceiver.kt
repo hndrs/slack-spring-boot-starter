43351128f89d8b37dff4b7cb3d0f8cb4dcf94646
Ben Denger <ben@kreait.com>
2019-12-12 15:02:42 +0100

    removed typedAdapter due to too little benefits

--- starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/store/user/UserJoinedEventReceiver.kt
+++ starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/store/user/UserJoinedEventReceiver.kt
@@ -2,7 +2,7 @@ package com.kreait.slack.broker.store.user
 
 import com.kreait.slack.api.contract.jackson.event.Event
 import com.kreait.slack.api.contract.jackson.event.SlackEvent
-import com.kreait.slack.broker.receiver.adapter.TypedEventReceiverAdapter
+import com.kreait.slack.broker.receiver.TypedEventReceiver
 import com.kreait.slack.broker.store.team.Team
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
@@ -10,7 +10,11 @@ import org.springframework.http.HttpHeaders
 /**
  * EventReceiver that adds a user to the registered store when a new user joins the team
  */
-class UserJoinedEventReceiver @Autowired constructor(private val userStore: UserStore) : TypedEventReceiverAdapter<Event.TeamJoin>(Event.TeamJoin.TYPE) {
+class UserJoinedEventReceiver @Autowired constructor(private val userStore: UserStore) : TypedEventReceiver<Event.TeamJoin> {
+
+    override fun supportsEvent(slackEvent: SlackEvent<Event>): Boolean {
+        return slackEvent.type == Event.TeamJoin.TYPE
+    }
 
     override fun onReceive(slackEvent: SlackEvent<Event.TeamJoin>, headers: HttpHeaders, team: Team) {
         userStore.put(userOfMember(slackEvent.event.member))
