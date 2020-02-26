317d5d0b3e56b91f5d1bf223c6b973aff231d6de
Marvin Schramm <marvin@kreait.com>
2020-02-26 12:09:09 +0100

    simplify generic typing on events

--- starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/receiver/EventReceiver.kt
+++ starter/slack-spring-boot/src/main/kotlin/com/kreait/slack/broker/receiver/EventReceiver.kt
@@ -1,6 +1,5 @@
 package com.kreait.slack.broker.receiver
 
-import com.kreait.slack.api.contract.jackson.event.Event
 import com.kreait.slack.api.contract.jackson.event.SlackEvent
 import com.kreait.slack.broker.store.team.Team
 import org.springframework.core.Ordered
@@ -9,14 +8,14 @@ import org.springframework.http.HttpHeaders
 /**
  * EventReceiver that should be implemented to react to incoming events
  */
-interface EventReceiver<T : Event> {
+interface EventReceiver {
 
     /**
      * Method that determines if the incoming event should be handled by the implementing receiver
      *
      * @param slackEvent the incoming event
      */
-    fun supportsEvent(slackEvent: SlackEvent<T>): Boolean = true
+    fun supportsEvent(slackEvent: SlackEvent): Boolean = true
 
     /**
      * will be executed if [supportsEvent] returned true
@@ -24,7 +23,7 @@ interface EventReceiver<T : Event> {
      * @param slackEvent the incoming event
      * @param team the team with the access-token which can be used for further actions
      */
-    fun onReceiveEvent(slackEvent: SlackEvent<T>, headers: HttpHeaders, team: Team)
+    fun onReceiveEvent(slackEvent: SlackEvent, headers: HttpHeaders, team: Team)
 
     /**
      * Method that determines if an exception in this receiver should be thrown
