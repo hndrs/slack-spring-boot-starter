89f1e12012129dabfad325cb007e46829235baca
Marvin Schramm <marvin@kreait.com>
2020-12-07 21:37:29 +0100

    fix wrong test import

--- samples/slack-spring-boot-starter-sample/src/test/kotlin/com/kreait/slack/sample/SampleApplicationTests.kt
+++ samples/slack-spring-boot-starter-sample/src/test/kotlin/com/kreait/slack/sample/SampleApplicationTests.kt
@@ -1,16 +1,13 @@
 package com.kreait.slack.sample
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class SampleApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
