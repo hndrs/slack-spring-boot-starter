43161a82ee8e13d0b097e5b073771905a77a313e
Marvin Schramm <marvin@kreait.com>
2020-12-10 09:12:06 +0100

    Removes obsolete dtos and test extensions

--- data/slack-jackson-dto-test-extensions/src/main/kotlin/com/kreait/slack/api/contract/jackson/group/channels/ChannelsUnarchiveExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.kreait.slack.api.contract.jackson.group.channels
-
-fun ChannelsArchiveRequest.Companion.sample(): ChannelsArchiveRequest = ChannelsArchiveRequest("")
-
-fun ErrorChannelArchiveResponse.Companion.sample(): ErrorChannelArchiveResponse = ErrorChannelArchiveResponse(false, "")
-
-fun SuccessfulChannelArchiveResponse.Companion.sample(): SuccessfulChannelArchiveResponse =
-    SuccessfulChannelArchiveResponse(true)
