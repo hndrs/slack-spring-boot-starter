666f2583520f98cd4fc54a937a7177985c463a1d
Marvin Schramm <marvin@kreait.com>
2020-12-12 15:12:56 +0100

    remove non capitalize

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -11,7 +11,7 @@ danger(args) {
 
         // Changelog
         if (!isTrivial && !changelogChanged && sourceChanges != null) {
-            warn(WordUtils.capitalize("any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md)."))
+            warn("Any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).")
         }
 
         // Big PR Check
