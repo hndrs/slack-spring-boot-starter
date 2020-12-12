e1cc034a9f0a2035295acc4f4b59e460519f9601
Marvin Schramm <marvin@kreait.com>
2020-12-12 18:02:42 +0100

    try source

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -4,14 +4,15 @@ danger(args) {
 
     val allSourceFiles = git.modifiedFiles + git.createdFiles
     val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
-    allSourceFiles.forEach {
-        println(it)
-    }
+    val sourceChanges = allSourceFiles.firstOrNull { it.contains("/starter") }
+
 
     onGitHub {
         val isTrivial = pullRequest.title.contains("#trivial")
 
+        allSourceFiles.forEach {
+            message(it)
+        }
         // Changelog
         if (!isTrivial && !changelogChanged && sourceChanges != null) {
             warn("Any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).")
